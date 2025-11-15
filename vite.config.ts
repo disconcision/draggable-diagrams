81c185fe6ac652a63cf559a4c9e6ba719a6f07ef
Joshua Horowitz <joshuah@alum.mit.edu>
2025-11-14 21:42:53 -0800

    vite react plugin at last

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { qrcodePlugin } from "./vite-plugin-qrcode";
 
 export default defineConfig({
   base: "./",
-  plugins: [tailwindcss(), qrcodePlugin()],
+  plugins: [react(), tailwindcss(), qrcodePlugin()],
   define: {
     "process.env": {},
   },
