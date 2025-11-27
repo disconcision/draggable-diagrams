disconcision/draggable-diagrams

commit cbeaede59e4548455e3fcc7f2924f93ac5bfbb25

clean up drag-spec stuff, some immer

src/manipulable.ts
@@ -11,7 +11,7 @@ import {
   lerpDiagrams,
   lerpDiagrams3,
 } from "./shape";
-import { assert, assertNever, pipe } from "./utils";
+import { assert, assertNever, hasKey, Many, manyToArray, pipe } from "./utils";
 import { Vec2 } from "./vec2";
 // @ts-ignore
 import { minimize } from "./minimize";
@@ -41,7 +41,7 @@ export type ManipulableBase<T, ManipulableConfig> = {
     state: T,
     draggableKey: string,
     config: ManipulableConfig,
-  ): AccessibleFromReturn<T>;
+  ): DragSpec<T>;
   sourceFile?: string;
 };
 
@@ -81,11 +81,40 @@ export function manipulableDefaultConfig<T, ManipulableConfig>(
  * single manifold, or a bunch of manifolds each going to a single
  * new state. Hmm.
  */
-export type AccessibleFromReturn<T> =
-  | T[]
-  | { manifolds: T[][] }
-  | { initParams: number[]; stateFromParams: (...params: number[]) => T }
-  | { paramPaths: PathIn<T, number>[] };
+
+// TODO: more sophisticated combos
+export type DragSpec<T> = Many<DragSpecManifold<T>> | DragSpecParams<T>;
+
+export type DragSpecManifold<T> = { type: "manifold"; states: T[] };
+
+export type DragSpecParams<T> =
+  | { type: "param-paths"; paramPaths: PathIn<T, number>[] }
+  | {
+      type: "params";
+      initParams: number[];
+      stateFromParams: (...params: number[]) => T;
+    };
+
+export function span<T>(states: T[]): DragSpecManifold<T> {
+  return { type: "manifold", states };
+}
+export function straightTo<T>(state: T): DragSpecManifold<T> {
+  return { type: "manifold", states: [state] };
+}
+export function params<T>(
+  initParams: number[],
+  stateFromParams: (...params: number[]) => T,
+): DragSpecParams<T> {
+  return { type: "params", initParams, stateFromParams };
+}
+export function numsAtPaths<T>(
+  paramPaths: PathIn<T, number>[],
+): DragSpecParams<T> {
+  return { type: "param-paths", paramPaths };
+}
+export function numAtPath<T>(paramPath: PathIn<T, number>): DragSpecParams<T> {
+  return { type: "param-paths", paramPaths: [paramPath] };
+}
 
 export type Path = (string | number)[];
 
@@ -371,32 +400,32 @@ export class ManipulableDrawer<T, Config = unknown> {
     pointerOffset: Vec2,
     manipulableConfig: Config,
   ) {
-    const r = this.manipulable.accessibleFrom(
+    const dragSpec = this.manipulable.accessibleFrom(
       state,
       draggableKey,
       manipulableConfig,
     );
 
-    if (typeof r === "object" && "initParams" in r) {
+    if (hasKey(dragSpec, "initParams")) {
       this.state = {
         type: "dragging-params",
         draggableKey,
         pointerOffset,
-        curParams: r.initParams,
-        stateFromParams: r.stateFromParams,
+        curParams: dragSpec.initParams,
+        stateFromParams: dragSpec.stateFromParams,
       };
       return;
     }
 
-    if (typeof r === "object" && "paramPaths" in r) {
+    if (hasKey(dragSpec, "paramPaths")) {
       this.state = {
         type: "dragging-params",
         draggableKey,
         pointerOffset,
-        curParams: r.paramPaths.map((path) => getAtPath(state, path)),
+        curParams: dragSpec.paramPaths.map((path) => getAtPath(state, path)),
         stateFromParams: (...params: number[]) => {
           let newState = state;
-          r.paramPaths.forEach((path, idx) => {
+          dragSpec.paramPaths.forEach((path, idx) => {
             newState = setAtPath(newState, path, params[idx]);
           });
           return newState;
@@ -405,11 +434,7 @@ export class ManipulableDrawer<T, Config = unknown> {
       return;
     }
 
-    const statesForManifolds = Array.isArray(r)
-      ? [r]
-      : r.manifolds.length > 0 // things go wrong if there are zero manifolds
-        ? r.manifolds
-        : [[]];
+    const manifoldSpecs = manyToArray(dragSpec);
 
     const makeManifoldPoint = (state: T): ManifoldPoint<T> => {
       const diagram = this.manipulable.render(
@@ -424,10 +449,10 @@ export class ManipulableDrawer<T, Config = unknown> {
 
     const startingPoint = makeManifoldPoint(state);
 
-    const manifolds = statesForManifolds.map((statesForManifold) => {
+    const manifolds = manifoldSpecs.map(({ states }) => {
       const points = [
         startingPoint,
-        ...statesForManifold
+        ...states
           .filter((s) => !_.isEqual(s, state))
           .map((state) => makeManifoldPoint(state)),
       ];

src/manipulable-angle.ts
@@ -1,4 +1,4 @@
-import { Manipulable } from "./manipulable";
+import { Manipulable, numAtPath } from "./manipulable";
 import { circle, group, line } from "./shape";
 import { Vec2 } from "./vec2";
 
@@ -32,9 +32,7 @@ export const manipulableAngle: Manipulable<AngleState> = {
   },
 
   accessibleFrom(_state, _draggableKey) {
-    return {
-      paramPaths: [["angle"]],
-    };
+    return numAtPath(["angle"]);
   },
 };
 

src/manipulable-clock.ts
@@ -1,4 +1,4 @@
-import { Manipulable } from "./manipulable";
+import { Manipulable, numAtPath } from "./manipulable";
 import { circle, Diagram, group, line } from "./shape";
 import { Vec2 } from "./vec2";
 
@@ -44,9 +44,7 @@ export const manipulableClock: Manipulable<ClockState> = {
   },
 
   accessibleFrom(_state, _draggableKey) {
-    return {
-      paramPaths: [["hours"]],
-    };
+    return numAtPath(["hours"]);
   },
 };
 

src/manipulable-fifteen.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { Manipulable } from "./manipulable";
+import { Manipulable, straightTo } from "./manipulable";
 import { group, rectangle } from "./shape";
 import { defined } from "./utils";
 import { Vec2 } from "./vec2";
@@ -44,34 +44,30 @@ export const manipulableFifteen: Manipulable<FifteenState> = {
     // if we're not blank, we can only swap with blank
     // if we are blank, we can swap with any neighbor
     const dragLoc = Vec2(state.tiles[draggableKey]);
-    return {
-      manifolds: (
-        [
-          [-1, 0],
-          [1, 0],
-          [0, -1],
-          [0, 1],
-        ] as const
-      )
-        .map((d) => {
-          const adjLoc = dragLoc.add(d);
-          if (!inXYWH(adjLoc, XYWH(0, 0, state.w - 1, state.h - 1))) return;
-          const adjTileKey = _.findKey(state.tiles, (t) => adjLoc.eq(t));
-          if (!adjTileKey) return;
-          if (!(draggableKey === " " || adjTileKey === " ")) return;
-          return [
-            {
-              ...state,
-              tiles: {
-                ...state.tiles,
-                [draggableKey]: adjLoc.xy(),
-                [adjTileKey]: dragLoc.xy(),
-              },
-            },
-          ];
-        })
-        .filter(defined),
-    };
+    return (
+      [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+      ] as const
+    )
+      .map((d) => {
+        const adjLoc = dragLoc.add(d);
+        if (!inXYWH(adjLoc, XYWH(0, 0, state.w - 1, state.h - 1))) return;
+        const adjTileKey = _.findKey(state.tiles, (t) => adjLoc.eq(t));
+        if (!adjTileKey) return;
+        if (!(draggableKey === " " || adjTileKey === " ")) return;
+        return straightTo({
+          ...state,
+          tiles: {
+            ...state.tiles,
+            [draggableKey]: adjLoc.xy(),
+            [adjTileKey]: dragLoc.xy(),
+          },
+        });
+      })
+      .filter(defined);
   },
 };
 

src/manipulable-flippy.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { Manipulable } from "./manipulable";
+import { Manipulable, straightTo } from "./manipulable";
 import { circle, group, line, rectangle } from "./shape";
 import { Vec2 } from "./vec2";
 import { XYWH } from "./xywh";
@@ -68,13 +68,11 @@ export const manipulableFlippy: Manipulable<PermState> = {
     // the dragged key in place; it will be ignored cuz the identity
     // is always included.
     const reversed = [...state.perm].reverse();
-    return {
-      manifolds: _.range(state.perm.length).map((idx) => [
-        {
-          perm: [...reversed.slice(idx), ...reversed.slice(0, idx)],
-        },
-      ]),
-    };
+    return _.range(state.perm.length).map((idx) =>
+      straightTo({
+        perm: [...reversed.slice(idx), ...reversed.slice(0, idx)],
+      }),
+    );
   },
 };
 

src/manipulable-graph.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { Manipulable } from "./manipulable";
+import { Manipulable, numsAtPaths, span } from "./manipulable";
 import { circle, group, line, polygon } from "./shape";
 import { Vec2 } from "./vec2";
 
@@ -123,14 +123,12 @@ export const manipulableGraph: Manipulable<GraphState> = {
       }
     } else if (draggableKey.startsWith("node-")) {
       const nodeKey = draggableKey.slice("node-".length);
-      return {
-        paramPaths: [
-          ["nodes", nodeKey, "x"],
-          ["nodes", nodeKey, "y"],
-        ],
-      };
+      return numsAtPaths([
+        ["nodes", nodeKey, "x"],
+        ["nodes", nodeKey, "y"],
+      ]);
     }
-    return newStates;
+    return span(newStates);
   },
 };
 

src/manipulable-grid-poly.ts
@@ -1,7 +1,8 @@
+import { produce } from "immer";
 import _ from "lodash";
-import { Manipulable } from "./manipulable";
+import { Manipulable, span } from "./manipulable";
 import { circle, group, line } from "./shape";
-import { setImm } from "./utils";
+import { assert } from "./utils";
 import { Vec2 } from "./vec2";
 
 type GridPolyState = {
@@ -52,19 +53,20 @@ export const manipulableGridPoly: Manipulable<GridPolyState> = {
 
   accessibleFrom(state, draggableKey) {
     const idx = parseInt(draggableKey, 10);
-    if (!isNaN(idx)) {
-      const availablePositions = _.range(state.w)
-        .flatMap((x) => _.range(state.h).map((y) => ({ x, y })))
-        .filter(
-          (pos) => !state.points.some((pt) => pt.x === pos.x && pt.y === pos.y),
-        );
-      return availablePositions.map((newPos) => ({
-        ...state,
-        points: setImm(state.points, idx, { x: newPos.x, y: newPos.y }),
-      }));
-    } else {
-      return [];
+    assert(!isNaN(idx));
+    const states = [];
+    for (const x of _.range(state.w)) {
+      for (const y of _.range(state.h)) {
+        if (!state.points.some((pt) => pt.x === x && pt.y === y)) {
+          states.push(
+            produce(state, (draft) => {
+              draft.points[idx] = { x, y };
+            }),
+          );
+        }
+      }
     }
+    return span(states);
   },
 };
 

src/manipulable-insert-and-remove.ts
@@ -1,7 +1,7 @@
+import { produce } from "immer";
 import _ from "lodash";
-import { Manipulable } from "./manipulable";
+import { Manipulable, span } from "./manipulable";
 import { group, rectangle } from "./shape";
-import { insertImm, removeImm } from "./utils";
 import { Vec2 } from "./vec2";
 import { XYWH } from "./xywh";
 
@@ -54,28 +54,28 @@ export const manipulableInsertAndRemove: Manipulable<PermState> = {
     const itemIdx = state.items.findIndex((item) => item.key === draggableKey);
     if (itemIdx !== -1) {
       const draggedItem = state.items[itemIdx];
-      const itemsWithoutDragged = removeImm(state.items, itemIdx);
-
-      return [
-        ..._.range(itemsWithoutDragged.length + 1).map((idx) => ({
-          ...state,
-          items: insertImm(itemsWithoutDragged, idx, draggedItem),
-        })),
-        // TODO: handle deletion
-        // {
-        //   ...state,
-        //   items: itemsWithoutDragged,
-        // },
-      ];
+      return span(
+        _.range(state.items.length).map((idx) =>
+          produce(state, (draft) => {
+            draft.items.splice(itemIdx, 1);
+            draft.items.splice(idx, 0, draggedItem);
+          }),
+        ),
+      );
     } else {
       // item is from store, can be inserted anywhere
-      const storeItem = state.store.find((item) => item.key === draggableKey)!;
-      return _.range(state.items.length + 1).map((idx) => ({
-        store: state.store.map((item) =>
-          item.key === draggableKey ? { ...item, key: item.key + "-1" } : item,
+      const storeItemIdx = state.store.findIndex(
+        (item) => item.key === draggableKey,
+      );
+      const storeItem = state.store[storeItemIdx];
+      return span(
+        _.range(state.items.length + 1).map((idx) =>
+          produce(state, (draft) => {
+            draft.items.splice(idx, 0, storeItem);
+            draft.store[storeItemIdx].key += "-1";
+          }),
         ),
-        items: insertImm(state.items, idx, storeItem),
-      }));
+      );
     }
   },
 };

commit db182b26ebc5d1fe9c5e52a9599f6559cc2b7c6f

fix no-manifolds breakage from cbeaede5 (w/pte)

src/manipulable.ts
@@ -455,7 +455,10 @@ export class ManipulableDrawer<T extends object, Config = unknown> {
       return;
     }
 
-    const manifoldSpecs = manyToArray(dragSpec);
+    const manifoldSpecs = pipe(
+      manyToArray(dragSpec),
+      (arr) => (arr.length === 0 ? [span([])] : arr), // things go wrong if no manifolds
+    );
 
     const makeManifoldPoint = (state: T): ManifoldPoint<T> => {
       const diagram = this.manipulable.render(